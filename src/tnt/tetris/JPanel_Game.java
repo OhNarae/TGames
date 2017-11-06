429fc5989832e80f3f2528e1d4fe0d089452d378
OhNarae <oh@nate.com>
2017-11-06 17:50:33 +0900

    [2017-11-06][OhNarae]
    - 블럭 움직이기
    - 시간 라벨 기능 추가

---
+++ src/tnt/tetris/JPanel_Game.java
@@ -0,0 +1,86 @@
+package tnt.tetris;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JPanel;
+
+import tnt.tetris.JPanel_Game.Point;
+
+class JPanel_Game extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	class Point {
+		int x;
+		int y;
+
+		Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	Point[] blockPoint;
+
+	JPanel_Game() {
+		blockPoint = new Point[4];
+		blockPoint[0] = new Point(0, 0);
+		blockPoint[1] = new Point(0, 1);
+		blockPoint[2] = new Point(0, 2);
+		blockPoint[3] = new Point(0, 3);
+	}
+
+	public void keyProcessing(int vkDown) {
+		int nowKey = vkDown;
+
+		switch (nowKey) {
+		case KeyEvent.VK_DOWN:
+			for (Point points : blockPoint) {
+				points.y += 1;
+			}
+			break;
+		case KeyEvent.VK_UP:
+			for (Point points : blockPoint) {
+				points.y -= 1;
+			}
+			break;
+		case KeyEvent.VK_LEFT:
+			for (Point points : blockPoint) {
+				points.y += 1;
+			}
+			break;
+		case KeyEvent.VK_RIGHT:
+			for (Point points : blockPoint) {
+				points.y += 1;
+			}
+			break;	
+		}
+
+		repaint();
+	}
+
+	Image buffImage;
+	Graphics buffg;
+
+	public void paint(Graphics g) {
+		buffImage = createImage(getWidth(), getHeight());
+		buffg = buffImage.getGraphics();
+
+		buffg.clearRect(0, 0, getWidth(), getHeight());
+
+		int block_size = getWidth() / 10;
+
+		buffg.setColor(Color.YELLOW);
+		for (Point points : blockPoint) {
+			buffg.fillRect(points.x * block_size, points.y * block_size, block_size, block_size);
+		}
+
+		g.drawImage(buffImage, 0, 0, this);
+	}
+}
--- src/tnt/tetris/JPanel_Tetris.java
+++ src/tnt/tetris/JPanel_Tetris.java
@@ -16,6 +16,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.border.EmptyBorder;
@@ -32,142 +35,56 @@ import java.awt.Font;
 
 import tnt.*;
 
-class JPanel_Game extends JPanel implements KeyListener {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	class Point {
-		int x;
-		int y;
-
-		Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-
-	Point[] blockPoint;
-
-	private Action down = new AbstractAction("DOWN") {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 2042513262173601539L;
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			for (Point points : blockPoint) {
-				points.y += 1;
-			}
-			repaint();
-		}
-	};
-
-	private class MotionAction extends AbstractAction implements ActionListener
-	{
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 4169247310119459272L;
-		String name;
-	    public MotionAction(String name)
-	    {
-	        super(name);
-	        this.name = name;
-	    }
-	 
-	    public void actionPerformed(ActionEvent e)
-	    {
-	    	switch(this.name) {
-	    	case "DOWN":
-				for (Point points : blockPoint) {
-					points.y += 1;
-				}break;
-	    	case "UP":
-				for (Point points : blockPoint) {
-					points.y -= 1;
-				}break;	
-	    	}
-			repaint();
-	    }
-	}
-	
-	public MotionAction addAction(String name)
-	{
-		MotionAction action = new MotionAction(name);
-	 
-	    KeyStroke pressedKeyStroke = KeyStroke.getKeyStroke(name);
-	    InputMap inputMap = getInputMap(WHEN_IN_FOCUSED_WINDOW);
-	    inputMap.put(pressedKeyStroke, name);
-	    getActionMap().put(name, action);
-	 
-	    return action;
-	}
-	
-	JPanel_Game() {
-		addAction("DOWN");
-		addAction("UP");
-//		GameFrame.Inst.RegisterListner(this);
-        
-		blockPoint = new Point[4];
-		blockPoint[0] = new Point(0, 0);
-		blockPoint[1] = new Point(0, 1);
-		blockPoint[2] = new Point(0, 2);
-		blockPoint[3] = new Point(0, 3);
-	}
-
-	@Override
-	public void keyPressed(KeyEvent arg0) {
-		int nowKey = arg0.getKeyCode();
-
-		if (nowKey == KeyEvent.VK_DOWN) {
-			for (Point points : blockPoint) {
-				points.y += 1;
-			}
-			repaint();
-		}
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	Image buffImage;
-	Graphics buffg;
-
-	public void paint(Graphics g) {
-		buffImage = createImage(getWidth(), getHeight());
-		buffg = buffImage.getGraphics();
-
-		buffg.clearRect(0, 0, getWidth(), getHeight());
-
-		int block_size = getWidth() / 10;
-
-		buffg.setColor(Color.YELLOW);
-		for (Point points : blockPoint) {
-			buffg.fillRect(points.x * block_size, points.y * block_size, block_size, block_size);
-		}
-
-		g.drawImage(buffImage, 0, 0, this);
-	}
-}
-
 class JPanel_TBody extends JPanel {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1712487684772738792L;
+
+//	JLabel lblTime;
+	JLabel lblLevel;
+	JPanel pnNext;
+	public JPanel_Game panel_game;
+
+	class JLabel_Time extends JLabel{
+		private int sec = 0;
+		private int min = 0;
+		private int hour = 0;
+		
+		Timer timer;
+
+		JLabel_Time(){
+			setOpaque(true);
+			setBackground(Color.BLACK);
+			setForeground(Color.WHITE);
+			setFont(new Font(Statics.fontKorean, Font.PLAIN, 20));
+			setBorder(new MatteBorder(10, 20, 10, 20, Color.BLACK));
+			
+			Timer timer = new Timer();
+			TimerTask task = new TimerTask() {	
+				@Override
+				public void run() {
+					plus();
+					setText(String.format("%02d:%02d:%02d", hour, min, sec));
+				}			
+			};
+			timer.schedule(task, 1000, 1000);
+		}
+		
+		public void start() {
+			
+		}
+		
+		public void plus() {
+			if (sec < 59)
+				sec++;
+			else {
+				if (min < 59)
+					min++;
+				else {
+					hour++;
+					min = 0;
+				}
+				sec = 0;
+			}
+		}
+	}
 
 	public JPanel_TBody() {
 		setLayout(new BorderLayout(0, 0));
@@ -178,17 +95,12 @@ class JPanel_TBody extends JPanel {
 		panel_tetris2_info.setLayout(new BoxLayout(panel_tetris2_info, BoxLayout.Y_AXIS));
 		panel_tetris2_info.setBorder(new MatteBorder(20, 20, 20, 20, Statics.colorBackGround));
 
-		JLabel lblTime = new JLabel("00:00:00");
-		lblTime.setOpaque(true);
-		lblTime.setBackground(Color.BLACK);
-		lblTime.setForeground(Color.WHITE);
-		lblTime.setFont(new Font(Statics.fontKorean, Font.PLAIN, 20));
-		lblTime.setBorder(new MatteBorder(10, 20, 10, 20, Color.BLACK));
+		JLabel_Time lblTime = new JLabel_Time();
 		panel_tetris2_info.add(lblTime);
-
+		
 		panel_tetris2_info.add(Box.createVerticalStrut(25));
 
-		JLabel lblLevel = new JLabel("Lv.01");
+		lblLevel = new JLabel("Lv.01");
 		lblLevel.setOpaque(true);
 		lblLevel.setBackground(Color.BLACK);
 		lblLevel.setForeground(Color.WHITE);
@@ -204,7 +116,7 @@ class JPanel_TBody extends JPanel {
 		lblNext.setBorder(new MatteBorder(0, 20, 0, 0, (Color) new Color(255, 214, 109)));
 		panel_tetris2_info.add(lblNext);
 
-		JPanel pnNext = new JPanel();
+		pnNext = new JPanel();
 		pnNext.setBackground(Color.BLACK);
 		panel_tetris2_info.add(pnNext);
 
@@ -214,7 +126,7 @@ class JPanel_TBody extends JPanel {
 		panel_tetris2_game_border.setBorder(new MatteBorder(20, 20, 20, 20, Statics.colorBackGround));
 		panel_tetris2_game_border.setBackground(Color.BLACK);
 
-		JPanel_Game panel_game = new JPanel_Game();
+		panel_game = new JPanel_Game();
 		panel_tetris2_game_border.add(panel_game);
 	}
 }
@@ -224,6 +136,9 @@ public class JPanel_Tetris extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	JPanel_TBody panel_tetris1_body;
+	JPanel_TBody panel_tetris2_body;
 
 	public JPanel_Tetris() {
 		setLayout(new BorderLayout(0, 0));
@@ -255,10 +170,57 @@ public class JPanel_Tetris extends JPanel {
 		add(panel_body, BorderLayout.CENTER);
 		panel_body.setLayout(new BoxLayout(panel_body, BoxLayout.X_AXIS));
 
-		JPanel_TBody panel_tetris1_body = new JPanel_TBody();
+		panel_tetris1_body = new JPanel_TBody();
 		panel_body.add(panel_tetris1_body);
 
-		JPanel_TBody panel_tetris2_body = new JPanel_TBody();
+		panel_tetris2_body = new JPanel_TBody();
 		panel_body.add(panel_tetris2_body);
+
+		String[] Actions = { "DOWN", "UP", "LEFT", "RIGHT" };
+		for (String Action : Actions) {
+			addAction(Action);
+		}
 	}
+
+	public MotionAction addAction(String name) {
+		MotionAction action = new MotionAction(name);
+
+		KeyStroke pressedKeyStroke = KeyStroke.getKeyStroke(name);
+		InputMap inputMap = getInputMap(WHEN_IN_FOCUSED_WINDOW);
+		inputMap.put(pressedKeyStroke, name);
+		getActionMap().put(name, action);
+
+		return action;
+	}
+
+	private class MotionAction extends AbstractAction implements ActionListener {
+		String name;
+
+		public MotionAction(String name) {
+			super(name);
+			this.name = name;
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			switch (this.name) {
+			case "DOWN":
+				panel_tetris1_body.panel_game.keyProcessing(KeyEvent.VK_DOWN);
+				panel_tetris2_body.panel_game.keyProcessing(KeyEvent.VK_DOWN);
+				break;
+			case "UP":
+				panel_tetris1_body.panel_game.keyProcessing(KeyEvent.VK_UP);
+				panel_tetris2_body.panel_game.keyProcessing(KeyEvent.VK_UP);
+				break;
+			case "LEFT":
+				panel_tetris1_body.panel_game.keyProcessing(KeyEvent.VK_LEFT);
+				panel_tetris2_body.panel_game.keyProcessing(KeyEvent.VK_LEFT);
+				break;
+			case "RIGHT":
+				panel_tetris1_body.panel_game.keyProcessing(KeyEvent.VK_RIGHT);
+				panel_tetris2_body.panel_game.keyProcessing(KeyEvent.VK_RIGHT);
+				break;
+			}
+		}
+	}
+
 }
